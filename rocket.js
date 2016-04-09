61b26d5d5a5fa9a952da264d3bef5ec3075dd7ac
sagomilk <sagomilk@gmail.com>
2016-04-09 19:36:08 +0800

    rocket | Storage module

--- rocket.js
+++ rocket.js
@@ -1,10 +1,35 @@
-﻿;(function(window, document) {
-	
+﻿// 
+;(function(window, document) {
+	var localStorage = window.localStorage;
+
+	// not support localStorage
+	// exist the logic
+	if (!localStorage) {
+		return false;
+	}
+
 	var Rocket = {
 		version: "1.0"
 	}
 
+	// Storage module
+	// only for String value
+	var Storage = {
+		get: function(key) {
+			localStorage.getItem(key, value);
+		},
+		set: function(key, value) {
+			localStorage.setItem(key, value);
+		},
+		remove: function(key) {
+			localStorage.removeItem(key);
+		}		
+	}
+	
+
+	var inputArray = document.querySelectorAll("input");
+
 	//expose rocket variable to window
-	window.rocket = rocket;
+	window.Rocket = Rocket;
 
 })(window, document);
